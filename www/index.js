3e6ea74ed4b498412d236186293da28ff1f81761
kamaal111 <kamaal.f1@gmail.com>
2022-02-20 18:13:14 +0100

    Rendering game of life in canvas

--- www/index.js
+++ www/index.js
@@ -1,3 +1,14 @@
-import * as wasm from "wasm-game-of-life";
+import { Universe } from "wasm-game-of-life";
 
-wasm.greet("Kamaal");
+const pre = document.getElementById("game-of-life-canvas");
+const universe = Universe.new();
+
+const renderLoop = () => {
+  console.log("animating");
+  pre.textContent = universe.render();
+  universe.tick();
+
+  requestAnimationFrame(renderLoop);
+};
+
+requestAnimationFrame(renderLoop);
