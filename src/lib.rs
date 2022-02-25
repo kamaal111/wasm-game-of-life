0097aade6943211acca61ed78cd014fad6843efa
kamaal111 <kamaal.f1@gmail.com>
2022-02-25 19:10:25 +0100

    Refactoring inserting gliders

--- src/lib.rs
+++ src/lib.rs
@@ -105,40 +105,34 @@ impl Universe {
     }
 
     pub fn insert_glider(&mut self, row: u32, column: u32) {
-        let mut row = row;
-        if row == 0 {
-            row += 1;
-        } else if row == (self.height - 1) {
-            row -= 1;
-        }
-        let mut column = column;
-        if column == 0 {
-            column += 1;
-        } else if column == (self.width - 1) {
-            column -= 1;
-        }
+        let corrected_row = {
+            if row == 0 {
+                row + 1
+            } else if row == (self.height - 1) {
+                row - 1
+            } else {
+                row
+            }
+        };
+        let corrected_column = {
+            if column == 0 {
+                column + 1
+            } else if column == (self.width - 1) {
+                column - 1
+            } else {
+                column
+            }
+        };
 
-        let alive_indices: [usize; 5] = [
-            self.get_index(row - 1, column),     // Top
-            self.get_index(row, column + 1),     // right
-            self.get_index(row + 1, column + 1), // bottom right
-            self.get_index(row + 1, column),     // bottom
-            self.get_index(row + 1, column - 1), // bottom left
-        ];
-
-        for index in alive_indices {
-            self.cells.set(index, true);
-        }
-
-        let dead_indices: [usize; 4] = [
-            self.get_index(row, column),         // center
-            self.get_index(row, column - 1),     // left
-            self.get_index(row - 1, column - 1), // top left
-            self.get_index(row - 1, column + 1), // top right
-        ];
-
-        for index in dead_indices {
-            self.cells.set(index, false)
+        for x in 0..3 {
+            for y in 0..3 {
+                let index = self.get_index((corrected_row + x) - 1, (corrected_column + y) - 1);
+                let is_top_edges = x == 0 && y != 0;
+                let is_center = x == 1 && y == 1;
+                let is_left = x == 1 && y == 0;
+                self.cells
+                    .set(index, !is_top_edges && !is_center && !is_left)
+            }
         }
     }
 
