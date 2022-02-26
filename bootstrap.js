b9ed5eea317b34f0b7a3d766dadcb4aa65d379ce
kamaal111 <kamaal.f1@gmail.com>
2022-02-26 12:50:54 +0000

    Deploying to gh-pages from @ kamaal111/wasm-game-of-life@0ac0911bbb37cc0885aac9d63ba435e841232799 🚀

--- bootstrap.js
+++ bootstrap.js
@@ -174,7 +174,7 @@
 /******/ 				promises.push(installedWasmModuleData);
 /******/ 			else {
 /******/ 				var importObject = wasmImportObjects[wasmModuleId]();
-/******/ 				var req = fetch(__webpack_require__.p + "" + {"../pkg/wasm_game_of_life_bg.wasm":"ab7389bf23d471c1d373"}[wasmModuleId] + ".module.wasm");
+/******/ 				var req = fetch(__webpack_require__.p + "" + {"../pkg/wasm_game_of_life_bg.wasm":"c6d204ee68dce3244e1a"}[wasmModuleId] + ".module.wasm");
 /******/ 				var promise;
 /******/ 				if(importObject instanceof Promise && typeof WebAssembly.compileStreaming === 'function') {
 /******/ 					promise = Promise.all([WebAssembly.compileStreaming(req), importObject]).then(function(items) {
